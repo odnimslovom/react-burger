0bc33c15cd99f0a010194e09f7151ce8e39a4af9
Drew Paramonov <odnimslovom@icloud.com>
2022-08-17 20:23:48 +0300

    steps 3, 4: prepare file structure, git flow

---
+++ src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import appStyles from './app.module.css';
+
+const App = () => {
+  return (
+    <div className={appStyles.app}>
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from "./components/app/app";
+
+import  './vendor/normalize.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -7,5 +11,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
