9a1580d4eb2dfef63ad725aa917913fbbda2a37c
Drew Paramonov <odnimslovom@icloud.com>
2022-08-17 23:39:26 +0300

    step 5: AppHeader complete

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import AppHeader from "../app-header/app-header";
 
 import appStyles from './app.module.css';
 
 const App = () => {
   return (
     <div className={appStyles.app}>
-      <h1>Hello React</h1>
+      <AppHeader />
     </div>
   );
 }
